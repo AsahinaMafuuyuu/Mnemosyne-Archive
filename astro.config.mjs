49defdb5ae0ebd2d7a76325c1c8025df811055b1
AsahinaMafuuyuu <3366203917@qq.com>
2026-02-20 00:20:40 +0800

    add footer and modify some bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://asahinamafuyu/",
+  site: "https://asahinamafuyu.top/",
   integrations: [preact()],
 
   vite: {
