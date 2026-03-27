750c95f408ffa9dc9afe314b0fbfd864e96c71c2
AsahinaMafuuyuu <3366203917@qq.com>
2026-03-27 10:10:33 +0800

    fixed umami's bug

--- src/scripts/Umami_statistics.ts
+++ src/scripts/Umami_statistics.ts
@@ -14,7 +14,7 @@ export interface UmamiStats {
 }
 export async function getUmamiStats(path?: string): Promise<UmamiStats> {
     const res = await fetch(
-        `https://umami-request.woshizhendicai.workers.dev/api/umami/stats${path ? '?path=' + path : ''}`
+        `https://umami-request.asahinamafuyu.top/api/umami/stats${path ? '?path=' + path : ''}`
     )
         .then((res) => res.json())
         .then((data) => {
