829ffe3cee5e02513da88fc092c88d7717317581
AsahinaMafuuyuu <3366203917@qq.com>
2026-04-21 00:41:27 +0800

    fix: 将audio移动到public目录下，这样就能够解决src文件夹下对audio进行一个预处理的问题，从返回状态为200到206

---
+++ src/components/AudioPlayerComponents/AudioList.ts
@@ -0,0 +1,14 @@
+const AudioList = 
+{
+    AiDao:   '/assets/audios/AiDao.mp3',
+    AiPai:   '/assets/audios/AiPai.mp3',
+    EnvyBaby:   '/assets/audios/EnvyBaby.mp3',
+    Loschen:   '/assets/audios/Loschen.mp3',
+    Love:   '/assets/audios/Love.mp3',
+    NiceJune:   '/assets/audios/NiceJune.mp3',
+    ParasolCider:   '/assets/audios/ParasolCider.mp3',
+    PurpleSunflower:   '/assets/audios/PurpleSunflower.mp3',
+    ThreeColor:   '/assets/audios/ThreeColor.mp3'
+}
+
+export default AudioList;
